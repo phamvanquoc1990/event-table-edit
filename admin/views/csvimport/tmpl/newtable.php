3403302dc84d47023fee5985b8b9e6261a7ceb5a
Theophilix <theophilix@gmx.de>
2017-01-07 12:28:15 +0100

    Changed copyright information

--- admin/views/csvimport/tmpl/newtable.php
+++ admin/views/csvimport/tmpl/newtable.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * $Id:$
- * @copyright (C) 2007 - 2011 Manuel Kaspar
+ * @copyright (C) 2007 - 2017 Manuel Kaspar and Matthias  Gruhn
  * @license GNU/GPL, see LICENSE.php in the installation package
  * This file is part of Event Table Edit
  *
